0282106129fdd8fff95cd3719e6dbbf08b525d2a
Eric Ubaga <ubaga91@gmail.com>
2024-01-31 15:27:21 +0300

    Aded checkes for empty key

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -24,7 +24,7 @@ class BasicCache(BaseCaching):
         Assigns to the dictionary self.cache_data the item value for the key.
         If key or item is None, this method should not do anything.
         """
-        if key is not None:
+        if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
@@ -33,6 +33,6 @@ class BasicCache(BaseCaching):
         If key is None or if the key doesn't exist
         in self.cache_data, return None
         """
-        if key is None:
-            return None
-        return self.cache_data.get(key)
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
